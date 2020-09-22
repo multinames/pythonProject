dbafb68bcec80331b56b07eaef02490093f131dc
cepera <skuridin@gmail.com>
2020-09-22 16:26:31 +0300

    fix phyton's tests

--- venv/main/test4.py
+++ venv/main/test4.py
@@ -1,9 +1,9 @@
-with open('C:\\tmp\\file21.txt') as fin: #открываем исходный файл
-    fout = open('C:\\tmp\\file22.txt', 'w+') # открываем новый файл
-    flag = False # устанавливаем флаг
-    for line in fin: #читаем исходный файл
-        for tmp in fout: #читаем новый файд
-            if tmp == line: #сравниваем строки
+with open('C:\\tmp\\file21.txt') as fin:    # открываем исходный файл
+    fout = open('C:\\tmp\\file22.txt', 'w+')    # открываем новый файл
+    flag = False    # устанавливаем флаг
+    for line in fin:    # читаем исходный файл
+        for tmp in fout:    # читаем новый файд
+            if tmp == line: # сравниваем строки
                 flag = True
                 print('Совпадение найдено!')
                 break
@@ -11,5 +11,5 @@ with open('C:\\tmp\\file21.txt') as fin: #открываем исходный ф
             fout.write(line)
         elif flag == True:
             flag = False
-        fout.seek(0) # переход на начало нового файла во втором цикле
+        fout.seek(0)    # переход на начало нового файла во втором цикле
     fout.close()
