f3007a6c7548f79771168cb97fc7a6e0253adaef
cepera <skuridin@gmail.com>
2020-09-22 13:23:19 +0300

    fix test5.py

---
+++ venv/main/test5.py
@@ -0,0 +1,16 @@
+from tempfile import mkstemp
+from os import close
+from shutil import move
+
+file ='C:\\tmp\\file21.txt' #исходный файл
+file_out ='C:\\tmp\\file22.txt' # файл для записи
+
+file_temp, temp = mkstemp() # создать temp-файл
+lines = [] # уникальные строки из file
+with open(temp, 'w') as tf, open(file) as f:
+         for line in f: # читать file построчно
+             if line not in lines: # для line, отсутствующих в lines
+                 lines.append(line) # сохранить line в lines
+                 tf.write(line) # записать line в temp-файл
+close(file_temp) # закрыть temp-файл
+move(temp, file_out) # переместить/переименовать temp-файл в file
